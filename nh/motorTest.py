d67011e2e86777bf1e6031cdec4e3fd11d5dd3d6
James Lee <jml@jmlzone.com>
2019-07-16 16:08:17 -0400

    use spaceprobe as module for board and motor

--- nh/motorTest.py
+++ nh/motorTest.py
@@ -2,37 +2,11 @@
 import sys
 import time
 import RPi.GPIO as GPIO
-
-#Note: import motor and board config from spaceProbe, when updated.
-class motor(object) :
-    def __init__ (self) :
-        self.motorPins = [32,40,38,36]
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.motorPins, GPIO.OUT)
-        GPIO.output(self.motorPins,GPIO.LOW)    
-        self.step=0;
-        #self.steps=[[1,1,0,1], [1,0,0,1], [1,0,1,1], [0,0,1,1], [0,1,1,1], [0,1,1,0], [1,1,1,0], [1,1,0,0] ]
-        self.steps=[[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
-        self.reverse = 0
-    def go(self,num,dir) :
-        GPIO.output(self.motorPins,self.steps[self.step])
-        if dir <1 :
-            dir = -1
-        else :
-            dir = 1
-        if(self.reverse==0) :
-            dir = - dir
-        for i in range(num) :
-            self.step = (self.step + dir) % 8
-            GPIO.output(self.motorPins,self.steps[self.step])
-            time.sleep(0.002)
-        GPIO.output(self.motorPins,GPIO.LOW)    
-    def off(self) :
-        GPIO.output(self.motorPins,[0,0,0,0])
-
-#print "Number of arguments %d " % len(sys.argv)
-#print "argument list", str(sys.argv)
-#print "arg 1 = " + sys.argv[1]
+import spaceProbe as board
+#Note: import motor and board config from spaceProbe
+# detect board type
+b=board.board()
+m=board.motor(b.ioPinDict['cameraMotor'])
 
 # -1 = approach
 # +1 = depart
@@ -42,7 +16,7 @@ if(sys.argv[2] == 'depart') :
 else:
     dir = -1
 
-m=motor()
+
 m.reverse = 0
 startTime = time.time()
 m.go(steps,dir)
